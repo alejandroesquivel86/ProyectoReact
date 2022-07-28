d926d8a1fa274ef8dd19e45502508221ae8dd419
alejandroesquivel86 <miguelesquivel86@gmail.com>
2022-07-28 00:10:56 -0300

    CATÁLOGO CON MAPS

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,25 +1,46 @@
-import ItemProduct from "../ItemProduct/ItemProduct";
+import {useState} from "react";
 import './ItemListContainer.scss';
+import products from  "../util/productmock";
+import ItemList from "../ItemProduct/ItemList";
 
 const ItemListContainer = ({section}) => {
-    const product1 = {
-        title:"Presupuesto",
-        price: 1500,
-        image: 'presupuesto.png',
-        stock: 10
-    }
-    const product2 = {
-        title:"Consulta",
-        price: 3500,
-        image: 'presupuesto.png',
-        stock: 6
-    }
+    // const product1 = {
+    //     title:"Presupuesto",
+    //     price: 1500,
+    //     image: 'presupuesto.png',
+    //     stock: 10
+    // }
+    // const product2 = {
+    //     title:"Consulta",
+    //     price: 3500,
+    //     image: 'presupuesto.png',
+    //     stock: 6
+    // }
+
+    const [listProducts, setListProducts] = useState([])
+
+    const getProducts = new Promise ((resolve,reject) =>{
+        setTimeout(() => {
+            resolve(products)
+        }, 2000);
+    });
+    
+    getProducts
+    .then ((res) =>{
+        setListProducts(res)    
+    })
+    .catch ((bug) =>{
+        console.log("error capturado")    
+    })
+    .finally (() =>{
+        console.log("termino")    
+    })
+
     return(
         <div>
             <h2>{section}</h2>
                 <div className='list-products'>
-                    <ItemProduct data={product1}/>                    
-                    <ItemProduct data={product2}/>   
+                <ItemList dataProducts={listProducts}/>
                 </div>
         </div>
     )
---
+++ src/components/util/productmock.js
@@ -0,0 +1,18 @@
+const products=[
+    {
+        id: 1,
+        title:"Presupuesto",
+        price: 1500,
+        image: 'presupuesto.png',
+        stock: 10
+        },
+    {
+        id: 2,
+        title:"Consulta",
+        price: 3500,
+        image: 'presupuesto.png',
+        stock: 6
+        }
+]
+
+export default products
